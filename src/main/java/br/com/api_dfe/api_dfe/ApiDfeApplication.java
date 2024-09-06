8ab2df8c1b55021ed5b22f7c28c4a8c268f836c4
Artur Moises <arturmoiscontato@gmail.com>
2024-09-06 10:31:21 -0300

    initial commit

---
+++ src/main/java/br/com/api_dfe/api_dfe/ApiDfeApplication.java
@@ -0,0 +1,13 @@
+package br.com.api_dfe.api_dfe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiDfeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiDfeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api_dfe/api_dfe/ApiDfeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api_dfe.api_dfe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDfeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
